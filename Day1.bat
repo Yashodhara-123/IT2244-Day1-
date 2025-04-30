55bffa05ab333c5e9d5dc97aff3d031143e9b45b
Yashodhara-123 <sandaniyashodhara10@gmail.com>
2025-04-30 13:44:52 +0530

    Add files via upload

---
+++ Day1.bat
@@ -0,0 +1,29 @@
+mkdir Criteria_1
+ cd Criteria_1
+ mkdir Standard_1
+ mkdir Standard_2
+ mkdir Standard_3
+ mkdir Standard_4
+ cd..
+mkdir Criteria_2
+ cd Criteria_2
+ mkdir Standard_1
+ mkdir Standard_2
+ cd..
+mkdir Criteria_3
+ cd Criteria_3
+ mkdir Standard_1
+ mkdir Standard_2
+ mkdir Standard_3
+ mkdir Standard_4
+ mkdir Standard_5
+ mkdir Standard_6
+ cd..
+mkdir Criteria_4
+ cd Criteria_4
+ mkdir Standard_1
+ cd..
+mkdir Criteria_5
+mkdir Criteria_6
+mkdir Criteria_7
+mkdir Criteria_8
---
+++ day1_2.bat
@@ -0,0 +1,9 @@
+@echo off
+ echo Username: %USERNAME%
+ echo Windows Version: 
+ 
+ ver
+ 
+ echo date : %date%
+ echo time : %time%
+ pause
